eaa807d9331441c93e650fbf485a398684688b03
Miguel Mora Perea <miguel@malonetuning.com>
2019-06-04 12:47:29 -0700

    Fix subdir makefile

--- TDD/Tests/subdir.mk
+++ TDD/Tests/subdir.mk
@@ -20,7 +20,7 @@ CPP_DEPS += \
 Tests/%.o: ../Tests/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I"$(dir $(abspath $(CURDIR)))inc" -I"$(dir $(abspath $(CURDIR)))lib/cpputest/include}" -O0 -g3 -ftest-coverage -fprofile-arcs -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -I"$(dir $(abspath $(CURDIR)))inc" -I"$(dir $(abspath $(CURDIR)))lib/cpputest/include" -O0 -g3 -ftest-coverage -fprofile-arcs -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
